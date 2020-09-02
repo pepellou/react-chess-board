a82830d15f091e4d841031db86480b3a25b4d24a
Pepe Doval <pepellou@gmail.com>
2020-09-02 20:43:58 +0200

    Handler showcase for changing position

--- src/index.js
+++ src/index.js
@@ -4,18 +4,37 @@ import ReactDOM from 'react-dom';
 import Showcase from './Showcase/Showcase';
 import ChessBoard from './ChessBoard/ChessBoard';
 
+const articleStyles = {
+    float: 'left',
+    width: '100%'
+};
+
+function handlePositionChange(fen) {
+    if (fen == undefined) {
+        console.log('No FEN :(');
+    } else {
+        console.log(fen);
+    }
+};
+
 ReactDOM.render(
     <main>
-        <article style={{float: 'left', width: '100%'}}>
+        <article style={articleStyles}>
             <h2>Default board</h2>
             <Showcase>
                 <ChessBoard />
             </Showcase>
         </article>
-        <article style={{float: 'left', width: '100%'}}>
+        <article style={articleStyles}>
             <h2>Specifying a FEN</h2>
             <Showcase>
                 <ChessBoard fen='8/8/4kpp1/3p1b2/p6P/2B5/6P1/6K1 b - - 2 47' />
+            </Showcase>
+        </article>
+        <article style={articleStyles}>
+            <h2>Specifying a FEN</h2>
+            <Showcase>
+                <ChessBoard fen='8/8/4kpp1/3p1b2/p6P/2B5/6P1/6K1 b - - 2 47' onPositionChange={handlePositionChange} />
             </Showcase>
         </article>
     </main>,
